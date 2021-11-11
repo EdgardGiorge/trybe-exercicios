cd737b91a312233d9df5c3b068af482fec565102
EdgardGiorge <edgardgiorge@hotmail.com>
2021-11-11 10:07:10 -0300

    JavaScript - Primeiros passos

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/4.1-scripts.js
@@ -0,0 +1,62 @@
+//JS E LÓGICA DE PROGRAMAÇÃO
+//PRIMEIROS PASSOS - VARIÁVEIS
+// let age = 43; 
+// let fullName = "Edgard Giorge";
+// age = 42;
+// console.log(age);
+// console.log(fullName);
+
+//Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
+// let movie = "A Grande aposta"; //Tipo String
+// let score = 10; // tipo number. JS é dinâmico ele entende o q vc quer!
+// let isGood = true; //boolean. No console do HTML com typeof te mostra o tipo de variável! 
+// let name; // indefinida
+// let color = null; // nula, bom pra alterar, na sequência uma cor mais pra frente
+// let salary = 3500; // + - * / **
+// console.log(3 ** 3); // 3 elevado a 3 (exponencial)
+// salary+=1; // +=1 ou ++ incrementa 1 , -- decrementa 1
+//console.log(salary); //
+
+//Condições IF e ELSE, operadores de atribuição e operadores lógicos
+// if (condição) {
+//     código
+// } else if (outraCondição) {
+//     outro código
+// }
+//let trybe = 19.31; //Exemplo de operador &&
+// if (trybe >= 14 && trybe < 14.40) {
+//   console.log("Esquenta");    
+// }else if (trybe >= 16.20 && trybe < 18.00) {
+//   console.log("Aula ao vivo");  
+// }else if (trybe >= 19.30 && trybe < 20.00) {
+//     console.log("Fechamento"); 
+// }else {
+//   console.log("Fora dos momentos síncronos");    
+// }
+// const bebidaPrincipal =  'cafezinho'; //Exemplo de operador ||
+// const bebidaAlternativa = 'suco de laranja';
+// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+//   console.log("Obrigado por me atender :D")
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
+// console.log(!null); // true Exemplo de operador ! (not)
+// console.log(!undefined); // true
+
+// Switch e Case
+// let trafficLight; // está undefined de início
+// trafficLight = "verde"; // defini um valor
+// switch (trafficLight) { //chamei a variável
+//   case "vermelho":      // Se a variável for vermelho...
+//     console.log("pare");// Imprima pare
+//     break;              // Se a condição for verdadeira pare o comando
+//   case "amarelo":
+//     console.log("atenção");
+//     break;
+//   case "verde":
+//     console.log("siga");
+//     break;
+//   default:              // caso não atenda nenhuma condição
+//     console.log("valor não identificado");
+// }
+
