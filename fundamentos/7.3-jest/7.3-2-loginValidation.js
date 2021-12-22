fa01aa5eddaf7a94b40e50413aa6e2468d6e0fab
EdgardGiorge <edgardgiorge@hotmail.com>
2021-12-21 21:48:33 -0300

    Jest-Identif.erro e diff(expectativa e realidade)

---
+++ fundamentos/7.3-jest/7.3-2-loginValidation.js
@@ -0,0 +1,33 @@
+// loginValidation.js
+const greetingMessage = (user) => {
+  return `Hello, ${user}! Que bom ter você de volta`;
+};
+
+const loginErrorMessage = (user) => {
+  return `Pessoa usuária '${user}' não encontrada, tente novamente!`;
+};
+
+const user = {
+  userName: "Joana",
+  password: 123456,
+};
+
+const verifyCredentials = ({ userName, password }) => {
+  if (password === 123456 && userName === "Joana") {
+    return greetingMessage(userName);
+  } else {
+    return loginErrorMessage(userName);
+  }
+};
+
+const { userName, password } = user;
+
+module.exports = { greetingMessage, loginErrorMessage, verifyCredentials }
+
+//Vamos primeiro entender as funções que estão implementadas no arquivo loginValidation.js. Neste arquivo encontram-se três funções para simular uma situação de login. A função verifyCredentials faz uma validação simples se a pessoa usuária e a senha recebidas como argumento são estritamente iguais as chaves userName e password contidas no objeto user. Se essa verificação retornar true a função loginValidation vai chamar a função greetingMessage , que por sua vez vai retornar uma frase de boas vindas com o nome da pessoa usuária. Já no caso da função loginValidation retornar false , ou seja as credenciais inseridas não corresponderem com as existentes no objeto, ela vai chamar a função loginErrorMessage que irá por sua vez retornar um erro.
+
+//Execute no seu terminal o comando npm test e veja o que acontece. Ao rodar o teste você receberá a mensagem que um dos testes falhou.
+
+//Observe que, como dito anteriormente, mesmo existindo um teste com erro, os outros testes foram executados normalmente. Além disso, o log do erro gerado pelo jest te possibilita identificar rapidamente onde foi que ocorreu o erro, uma vez que é exibida a mensagem que está contida no teste em questão. Neste caso o erro ocorreu no teste contendo a descrição " verifyCredentials() calls the correct function depending on the user and password input".
+
+//Outro ponto positivo da utilização do Jest para fazer nossos testes é que ele traz uma mensagem contendo um diff, ou seja, o que era esperado de ocorrer no teste e o que de fato aconteceu. Isso nos ajuda a entender mais rapidamente onde está o erro. Neste exemplo o teste esperava receber como valor um objeto contendo uma pessoa usuária com o nome "Joana", mas ao invés disso recebeu "Bob". Sensacional, não é mesmo? Então bora pro conteúdo que na sequência vamos explicar o que são os expect e os matchers e como eles funcionam.
