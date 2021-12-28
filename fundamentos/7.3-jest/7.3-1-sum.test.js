b3a2e2df24fcad0eac81fe37683fa3eae54575c9
EdgardGiorge <edgardgiorge@hotmail.com>
2021-12-28 14:26:00 -0300

    matcher toBeCloseTo-pra num-flutuante

--- fundamentos/7.3-jest/7.3-1-sum.test.js
+++ fundamentos/7.3-jest/7.3-1-sum.test.js
@@ -5,11 +5,18 @@
 // touch .gitignore~enter~node_modules/~ctrl d~git add .gitignore~enter  , ignora o  node_modules no git
 // Arquivos com extensões .test ou spec são usados pra descrever o teste (sum.test.js). Arquivos js nós já conhecemos (sum.js)
 // Só iremos usar a função ou objeto, damos o seguinte dado: require('./nomeDoModulo';) ,recebe qual identificador do módulo que queremos importar, neste caso se estiver na mesma pasta, referencia o nome do arquivo sem a extensão ./conteudo1  
-// Já no arquivo js (sum.js), cada arquivo é um módulo, usamos o módulo do js "module" pra exportar uma função ou objeto com a propriedade export : modeule.export = "nome da função ou objeto";
+// Já no arquivo js (sum.js), cada arquivo é um módulo, usamos o módulo do js "module" pra exportar uma função ou objeto com a propriedade export : modeule.export = nome da função ou objeto;
 //const sum = (a, b) => a + b; // esta função descrita agora pode ser inserida em um arquivo js
 
 const sum = require('./7.3-1-sum');
+
+// function teste1() { // Modo sem arrow function
+//   expect(sum(2, 3)).toBe(5);
+// }
+// test('A soma de 2 + 3 deve ser igual a 5', teste1);
+
   test('sums two values', () => {
     expect(sum(2, 3)).toEqual(5);
   });
 
+  
