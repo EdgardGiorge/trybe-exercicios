feafb5e7af8ab5489292b1a13a428ce622199892
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-11 13:12:32 -0300

    Property Shorthand - Ao Vivo

--- fundamentos/8.5-6-property-shorthand.js
+++ fundamentos/8.5-6-property-shorthand.js
@@ -12,3 +12,19 @@ const getPosition = (latitude, longitude) => ({
   longitude});
 
 console.log(getPosition(-19.8157, -43.9542));
+
+// property shorthand
+//forma curta de escrever um objeto
+const nome = 'Ronald';
+const sobrenome = 'Lima';
+
+const nomeCompleto = {
+  nome,
+  sobrenome,
+};
+
+ console.log('nome completo', nomeCompleto);
+
+module.exports = {
+  nomeCompleto,
+};
