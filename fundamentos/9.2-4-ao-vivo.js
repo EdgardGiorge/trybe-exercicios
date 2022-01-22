30a62ba4c00fc445fc375daa702487ab460fc2f0
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-22 20:05:46 -0300

    HTML e CSS da aula ao vivo no arq. js

--- fundamentos/9.2-4-ao-vivo.js
+++ fundamentos/9.2-4-ao-vivo.js
@@ -44,3 +44,30 @@ async function listarPokemon() {
 }
 
 window.onload = listarPokemon;
+
+// Arquivo html ...
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <link rel="stylesheet" href="style.css">
+//   <title>Pokémons</title>
+// </head>
+// <body>
+//   <h1>Pokémons</h1>
+//   <ul></ul>
+
+//   <script src="script.js"></script>
+// </body>
+// </html>
+
+// Arquivo Css ...
+
+// li {
+//   text-decoration: none;
+//   display: flex;
+//   align-items: center;
+// }
