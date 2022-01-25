e1fac973dea25d0b77c22d5ce2e266b1946eb5bf
EdgardGiorge <edgardgiorge@hotmail.com>
2022-01-25 19:00:36 -0300

    Introção React - Exercícios

--- front-end/test/src/App.js
+++ front-end/test/src/App.js
@@ -44,3 +44,163 @@ function App() {
 
 
 export default App;
+
+/* Exercícios
+
+1- Crie um novo projeto utilizando npx create-react-app.
+
+Solução: npx create-react-app nomeDoSeuApp
+
+2- Crie uma lista de tarefas simples que:
+Utilize a função Task dentro do componente App .
+Leia as tarefas de dentro de um Array.
+Use a função map para criar cada item da lista no HTML.
+
+Solução:
+
+    import React from 'react';
+    import './App.css';
+
+    const Task = (value) => {
+      return (
+        <li key={value}>{value}</li>
+      );
+    }
+
+    const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+    class App extends React.Component {
+      render() {
+        return (
+          <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+        );
+      }
+    }
+
+    export default App;
+
+3- Crie um novo projeto utilizando npx create-react-app.
+a) Na pasta src, crie um novo arquivo chamado Header.jsx , que representará o componente Header;
+b) No arquivo Header.jsx , crie uma classe React, chamada Header. Essa classe deve renderizar uma tag h1 com o texto 'Conteúdos de Front-End'. Não esqueça de exportar esse componente;
+c) No arquivo App.js , importe o componente Header criado anteriormente;
+d) Renderize o componente Header no App.js ;
+e) Na pasta src, crie um novo arquivo chamado Content.jsx , que representará o componente Content ;
+f) Dentro do arquivo Content.jsx , crie uma classe React chamada Content ;
+g) Ainda no arquivo Content.jsx , adicione o seguinte array. A classe Content deve renderizar o array conteudos com a utilização da função map, como visto anteriormente, com o seguinte formato:
+
+O conteúdo é: `Nome do Conteúdo`
+Status: `Status do Conteúdo`
+Bloco: `Bloco do Conteúdo`
+
+Lembre-se de adicionar o atributo 'key' ao elemento pai dessa lista, na renderização.
+
+h) Exporte o componente Content ;
+i) No arquivo App.js , importe o componente Content criado anteriormente;
+j)Renderize o componente Content no App.js ;
+k)Crie um novo arquivo Footer.jsx ;
+l)No arquivo Footer.jsx , crie uma classe chamada Footer .
+A classe Footer deve renderizar uma tag h1 com o texto "E isso é só o começo...". Não esqueça de exportar o componente criado.
+m)Importe o componente Footer no app.js
+n)Renderize o componente Footer no app.js .
+
+
+Solução - App.js:
+      // App.js
+      import React from 'react';
+
+      import Header from './Header';
+      import Content from './Content';
+      import Footer from './Footer';
+
+      import './App.css'
+
+      class App extends React.Component {
+        render() {
+          return (
+            <div>
+              <Header />
+              <Content />
+              <Footer />
+            </div>
+          );
+        }
+      }
+
+      export default App;
+
+Solução - Header.jsx:
+      // Header.jsx
+      import React from 'react';
+
+      class Header extends React.Component {
+        render() {
+          return (
+            <div>
+              <h1 className='title'>Conteúdos de Front-end</h1>
+            </div>
+          )
+        }
+      }
+
+      export default Header;
+
+Solução - Content.jsx:
+      // Content.jsx
+      import React from 'react';
+
+      const conteudos = [
+        {
+          conteudo: 'High Order Functions',
+          bloco: 8,
+          status: 'Aprendido'
+        },
+        {
+          conteudo: 'Composição de Componentes',
+          bloco: 11,
+          status: 'Aprendendo',
+        },
+        {
+          conteudo: 'Composição de Estados',
+          bloco: 12,
+          status: 'Aprenderei'
+        },
+        {
+          conteudo: 'Redux',
+          bloco: 16,
+          status: 'Aprenderei'
+        },
+      ]
+      class Content extends React.Component {
+        render() {
+          return(
+            <div className="content">
+              {conteudos.map((elem) => (
+                <div key={elem.conteudo} className="card">
+                  <h4>{`O conteudo é: ${elem.conteudo}`}</h4>
+                  <p>{`status: ${elem.status}`}</p>
+                  <p>{`bloco: ${elem.bloco}`}</p>
+                </div>
+              ))}
+            </div>
+          );
+        }
+      }
+
+      export default Content;
+
+Solução - Footer.jsx:
+    // Footer.jsx
+      import React from 'react';
+
+      class Footer extends React.Component {
+        render() {
+          return (
+            <footer className='footer'>
+              <h1>E isso é só o começo...</h1>
+            </footer>
+          )
+        }
+      }
+
+      export default Footer; */      
+      
