256b375d26e3b36cb98225b513e9098a8a0496c6
EdgardGiorge <edgardgiorge@hotmail.com>
2022-05-22 23:45:41 -0300

    22.5 - Pacote express-rescue e Conclusão

--- back-end/22-4/erroHandlerExample.js
+++ back-end/22-4/erroHandlerExample.js
@@ -64,3 +64,82 @@ Agora, se você criar o arquivo e jogar o conteúdo, por exemplo, usando o coman
 Esse mesmo tipo de erro pode acontecer ao fazer uma query para um banco de dados, e ter várias possíveis falhas, como por exemplo: o banco não está respondendo a nosso pedido de conexão, temos uma query escrita errada, as credenciais de acesso ao banco estão erradas. Entre outras.
 Para que não seja necessário ter que criar estruturas try/catch sempre que formos utilizar códigos que eventualmente podem disparar exceções podemos usar um pacote chamado express-rescue.
 */
+
+/*
+        Pacote express-rescue
+O pacote express-rescue está disponível no npm e nos ajuda com a tarefa de garantir que os erros sempre sejam tratados. Para utilizá-lo, primeiro faça a instalação usando o comando npm i express-rescue
+Para adicionarmos os express-rescue, basta passarmos o nosso middleware como parâmetro para a função rescue que importamos do módulo. Essa função vai gerar um novo middleware que vai fazer o tratamento de erros da middleware sem precisarmos escrever o try/catch. Vamos refatorar o exemplo da seção anterior para usar o express-rescue.
+Copiar
+ errorHandlerExample.js 
+const express = require('express');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
+
+const app = express();
+
+app.get(
+  '/:fileName',
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: `Erro: ${err.message}` });
+});
+
+app.listen(3002);
+O que o novo middleware faz é simplesmente executar nosso middleware original dentro de um bloco de try ... catch. Caso ocorra qualquer erro no nosso middleware, esse erro é capturado pelo catch e passado para o next, dando início ao fluxo de erro do Express.
+Faça os mesmos testes que fizemos no final da seção anterior e vai perceber que o fluxo continua acontecendo da mesma forma, quando a excessão é disparada, a diferença é que nosso código ficou mais enxuto. Lembre-se de reiniciar a aplicação que estamos utilizando antes de testar novamente.
+Através do uso correto de middlewares de erro, é possível centralizar o tratamento de erros da aplicação em partes específicas dela. Isso facilita a construção dos middlewares de rotas, pois você não precisa ficar tratando erros em todos esses middlewares. Se algo der errado em qualquer rota que estiver envelopada pelo express-rescue, esse erro vai ser tratado pelo middleware de erros mais próximo.
+Por último, um padrão muito comum é ter um middleware de erro genérico, e outros middlewares que convertem erros para esse formato genérico. Por exemplo:
+Copiar
+ errorMiddleware.js 
+
+module.exports = (err, req, res, next) => {
+  if (err.code && err.status) {
+    return res.status(err.status).json({ message: err.message, code: err.code });
+  }
+
+  return res.status(500).json({ message: err.message });
+}
+O middleware acima verifica se o erro possui um código e um status HTTP. Caso possua, o código e a mensagem são devolvidas na response. Caso contrário um erro genérico de servidor é utilizado.
+Copiar
+ errorHandlerExample.js 
+const express = require('express');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
+
+const errorMiddleware = require('./errorMiddleware');
+
+const app = express();
+
+app.get('/:fileName', [
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  }),
+  (err, req, res, next) => {
+    if (err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
+
+    return next(err);
+  },
+]);
+
+app.use(errorMiddleware);
+app.listen(3002);
+Nesse trecho de código, convertemos um erro de leitura de arquivo para um erro que nosso middleware de erros conhece e sabe formatar. Dessa forma, nos middlewares comuns, precisamos nos preocupar apenas com o caminho feliz ao passo que, nos middlewares de erro, nos preocupamos apenas com o fluxo de erros.
+Repare, também, que estamos utilizando um Array para passar mais de um middleware para uma mesma rota. Poderíamos passar cada middleware como um parâmetro, mas um Array deixa mais explícita a intenção de, realmente, utilizarmos vários middlewares numa mesma rota.
+
+                Conclusão
+No conteúdo de hoje aprendemos o que são middlewares e diferente formas de associar um middleware com uma rota. Também entendemos como é possível passar valores entre middlewares e como organizar as rotas usando o recurso Router que permite quebrar uma aplicação express em partes menores, que ajuda bastante na organização do nosso código. Por fim, vimos como podemos tratar erros usando o middleware genérico de erro, e como escrever middlewares mais enxutos usando o express-rescue.
+Todos esses conceitos vão ser essenciais para nossos próximos passos para desenvolver aplicações web, usando o Node.js para construir APIs HTTP. Por isso é fundamental que você pratique bastante o desenvolvimento de APIs através dos exercícios e projeto desse bloco.
+
+*/
+
